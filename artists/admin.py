683c7b7b2a25419af8eb480095245cc2622ed260
AhmedE101 <ahmed2016170050@cis.asu.edu.eg>
2022-10-30 22:46:15 +0200

    modified

--- artists/admin.py
+++ artists/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Artist
 
 
-class display_admin(admin.ModelAdmin):
+class displayadmin(admin.ModelAdmin):
     list_display = ('stage_name', 'social_link', 'approved_albums')
 
 
 # Register your models here.
-admin.site.register(Artist, display_admin)
+admin.site.register(Artist, displayadmin)
