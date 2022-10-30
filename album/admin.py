a40d4236734b0fdcdb76e8284ffae86d16b25e46
AhmedE101 <ahmed2016170050@cis.asu.edu.eg>
2022-10-30 22:44:38 +0200

    modified

--- album/admin.py
+++ album/admin.py
@@ -3,13 +3,13 @@ from .models import Album
 # Register your models here.
 
 
-class approved_albumcolumn(admin.StackedInline):
+class approvedalbumcolumn(admin.StackedInline):
     model = Album
     extra = 1
 
 
-class readonly_album(admin.ModelAdmin):
+class readonlyalbum(admin.ModelAdmin):
     readonly_fields = ('creation_time',)
 
 
-admin.site.register(Album, readonly_album)
+admin.site.register(Album, readonlyalbum)
