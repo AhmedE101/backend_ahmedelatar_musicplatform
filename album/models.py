0a869e84b4e68e6dccef672a01e3a115583f22fb
AhmedE101 <ahmed2016170050@cis.asu.edu.eg>
2022-11-01 18:36:40 +0200

    fixed  minut issue

--- album/models.py
+++ album/models.py
@@ -14,11 +14,17 @@ class Album(models.Model):
         max_length=200, default="New Album", verbose_name="New Album")
 
     creation_time = models.DateTimeField(
-        'time created', unique=True, default=timezone)
+        'time created', unique=True)
 
     release_time = models.DateTimeField('publish time')
 
     album_cost = models.DecimalField(max_digits=20, decimal_places=2)
 
     def __str__(self):
-        return "name = " + self.album_name + "-->" + " Artist = " + self.artist_fk.stage_name
+        return "name = " + self.album_name + " Artist = " + self.artist_fk.stage_name
+
+    def was_released_today_or_before(self):
+        return self.release <= timezone.now()
+
+    def was_released_before_today(self):
+        return self.release <= timezone.now() - datetime.timedelta(days=1)
